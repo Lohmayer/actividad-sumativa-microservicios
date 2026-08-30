a2f35a7a36245ee274127d319dde868ec8bc92fa
Lohmayer <an.price@duocuc.cl>
2026-08-30 11:50:01 -0400

    Crear proyecto base del microservicio de publicaciones

---
+++ microservicio-publicaciones/src/main/java/com/duoc/microservicio_publicaciones/MicroservicioPublicacionesApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.microservicio_publicaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPublicacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPublicacionesApplication.class, args);
+	}
+
+}
---
+++ microservicio-publicaciones/src/test/java/com/duoc/microservicio_publicaciones/MicroservicioPublicacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.microservicio_publicaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPublicacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
