09435481ebee838bb1c0f185e60a431e33c1cc5e
Lohmayer <an.price@duocuc.cl>
2026-08-30 12:53:34 -0400

    Crear proyecto base del microservicio de mascotas

---
+++ microservicio-mascotas/src/main/java/com/duoc/microservicio_mascotas/MicroservicioMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.microservicio_mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioMascotasApplication.class, args);
+	}
+
+}
---
+++ microservicio-mascotas/src/test/java/com/duoc/microservicio_mascotas/MicroservicioMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.microservicio_mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
